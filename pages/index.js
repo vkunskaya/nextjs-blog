c64251504a7581465f5491019e98c700fbffa810
Volha <volha_kunskaya@epam.com>
2021-01-25 13:11:58 +0300

    text

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home({ allPostsData }) {
       </Head>
 
       <section className={utilStyles.headingMd}>
-        <p>Blablabla</p>
+        <p>Some text</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
